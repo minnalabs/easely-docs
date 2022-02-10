e7a4d6087a8519d44aee9b8d4bbf6ef0ccf04cba
Patrick Zhang <patrickpzzhang@gmail.com>
2022-02-09 23:58:38 -0800

    Fix edit this page link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/minnalabs/easely-docs',
+          editUrl: 'https://github.com/minnalabs/easely-docs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
